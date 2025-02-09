d205466d48058df1c5f7a5440f483e8073ae6364
kmolski <krzysztof.molski29@gmail.com>
2025-02-09 18:33:42 +0100

    frame: Add parser module

---
+++ src/frame/parser.rs
@@ -0,0 +1,109 @@
+use std::collections::HashMap;
+use std::str::from_utf8;
+
+use nom::bytes::complete::{take, take_while, take_while1};
+use nom::character::complete::{char as ch, line_ending};
+use nom::error::Error;
+use nom::multi::many0;
+use nom::sequence::{separated_pair, terminated};
+use nom::{AsChar, Finish, IResult, Parser};
+
+use crate::frame::{StompCommand, StompFrame, StompFrameError};
+
+macro_rules! stomp_command_parse_impl {
+    ($typename: ident, $($command:ident),+) => {
+        use nom::{IResult, Parser, branch::alt, bytes::complete::tag, combinator::value};
+        impl $typename {
+            fn parse(input: &[u8]) -> IResult<&[u8], $typename> {
+                alt((
+                    $(value(Self::$command, tag(stringify!($command)))),+
+                )).parse(input)
+            }
+        }
+    };
+}
+
+pub(super) use stomp_command_parse_impl;
+
+const HEADER_SEP: u8 = b':';
+
+fn is_header_octet(oct: u8) -> bool {
+    !matches!(oct, b'\r' | b'\n' | HEADER_SEP)
+}
+
+fn parse_header(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+    terminated(
+        separated_pair(
+            take_while1(is_header_octet),
+            ch(HEADER_SEP.as_char()),
+            take_while(is_header_octet),
+        ),
+        line_ending,
+    )
+    .parse(input)
+}
+
+fn collect_headers(
+    header_pairs: Vec<(&[u8], &[u8])>,
+) -> Result<HashMap<String, String>, StompFrameError> {
+    let mut headers = HashMap::with_capacity(header_pairs.len());
+    for pair in header_pairs {
+        // TODO: escape sequences
+        let key = from_utf8(pair.0)?.to_string();
+        let value = from_utf8(pair.1)?.to_string();
+        headers.entry(key).or_insert(value);
+    }
+    Ok(headers)
+}
+
+const CONTENT_LENGTH: &str = "content-length";
+
+fn parse_body_with_len(input: &[u8], body_len: usize) -> IResult<&[u8], &[u8]> {
+    terminated(take(body_len), (ch('\0'), many0(line_ending))).parse(input)
+}
+
+fn parse_body(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    terminated(
+        terminated(take_while(|c| c != b'\0'), ch('\0')),
+        many0(line_ending),
+    )
+    .parse(input)
+}
+
+fn parse_frame(
+    input: &[u8],
+) -> Result<(&[u8], (StompCommand, HashMap<String, String>, &[u8])), StompFrameError> {
+    let (rest, (command, header_pairs)) = (
+        terminated(StompCommand::parse, line_ending),
+        terminated(many0(parse_header), line_ending),
+    )
+        .parse(input)
+        .finish()?;
+    let headers = collect_headers(header_pairs)?;
+    let (rest, body) = if let Some(content_len) = headers.get(CONTENT_LENGTH) {
+        let body_len = content_len
+            .parse::<usize>()
+            .map_err(|e| StompFrameError::HeaderError(CONTENT_LENGTH.into(), e.to_string()))?;
+        parse_body_with_len(rest, body_len).finish()?
+    } else {
+        parse_body(rest).finish()?
+    };
+    Ok((rest, (command, headers, body)))
+}
+
+impl TryFrom<&[u8]> for StompFrame {
+    type Error = StompFrameError;
+
+    fn try_from(input: &[u8]) -> Result<Self, Self::Error> {
+        match parse_frame(input) {
+            Ok((_, (command, headers, body))) => StompFrame::new(command, headers, body),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+impl From<Error<&[u8]>> for StompFrameError {
+    fn from(value: Error<&[u8]>) -> Self {
+        StompFrameError::SyntaxError(String::from_utf8_lossy(value.input).to_string())
+    }
+}
